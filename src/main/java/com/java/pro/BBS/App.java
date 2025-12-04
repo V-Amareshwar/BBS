050c54937205291ede360803f7fc6cd84ebd2431
Konduri-CharanTeja <charantejakonduri@gmail.com>
2025-12-04 13:14:40 +0530

    firstz

---
+++ src/main/java/com/java/pro/BBS/App.java
@@ -0,0 +1,13 @@
+package com.java.pro.BBS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
